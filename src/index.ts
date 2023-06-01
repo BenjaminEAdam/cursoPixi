c2600c5e1b589335af935509a9ab10de315020b2
BenjaminEAdam <benjaadam14@gmail.com>
2023-06-01 18:51:11 -0300

    Ejercicio primera clase

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Assets, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1960,
+	height: 1080
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+Assets.add("clampy", "./clampy.png");
+Assets.add("pixi", "./pixi.png");
 
-clampy.anchor.set(0.5);
+Assets.load(["clampy", "pixi"]).then(() => {
+	
+	const pixi: Sprite = Sprite.from("pixi");
+	//const clampy: Sprite = Sprite.from("clampy.png");
+	
+	console.log("Hola mundo!", pixi.width, pixi.height);
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+	pixi.anchor.set(0.5);
+	
+	pixi.x = app.screen.width / 2;
+	pixi.y = app.screen.height / 2;
 
-app.stage.addChild(clampy);
+	app.stage.addChild(pixi);
+});
