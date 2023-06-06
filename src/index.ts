a63cb5e259173a3abf40f6a3b007bbb09a7f7b8e
BenjaminEAdam <80794769+BenjaminEAdam@users.noreply.github.com>
2023-06-06 14:48:19 -0300

    Agrego nuevas imágenes y el escalado sin funcionar todavía.

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,59 @@ const app = new Application({
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 1960,
-	height: 1080
+	width: 640,
+	height: 480
 });
+
+window.addEventListener("resize", ()=>{
+	console.log("resize done");
+	
+	const scaleWidth = Math.round(window.innerWidth / app.screen.width);
+	const scaleHeight = Math.round(window.innerHeight/app.screen.height);
+	const scaleScreen = Math.min(scaleWidth, scaleHeight);
+
+	const gameWhidth = Math.round(app.screen.width * scaleScreen);
+	const gameHeight = Math.round(app.screen.height * scaleScreen);
+
+	//const marginVertical = Math.floor((window.innerWidth - gameWhidth)/2);
+	//const marginHorizontal = Math.floor((window.innerHeight - gameHeight)/2);
+
+	if(app.view.style){
+		app.view.style.width = gameWhidth + "px";
+		app.view.style.height = gameHeight + "px";
+	}
+
+	
+
+
+});
+window.dispatchEvent(new Event("resize"));
+
 
 Assets.add("clampy", "./clampy.png");
 Assets.add("pixi", "./pixi.png");
+Assets.add("militar", "./militar.png");
+Assets.add("mortero", "./mortero.png");
 
-Assets.load(["clampy", "pixi"]).then(() => {
+Assets.load(["clampy", "pixi", "militar", "mortero"]).then(() => {
 	
-	const pixi: Sprite = Sprite.from("pixi");
+	const mortero: Sprite = Sprite.from("mortero");
+	const militar: Sprite = Sprite.from("militar");
+	//const pixi: Sprite = Sprite.from("pixi");
 	//const clampy: Sprite = Sprite.from("clampy.png");
 	
-	console.log("Hola mundo!", pixi.width, pixi.height);
-
-	pixi.anchor.set(0.5);
+	console.log("Hola mundo!", militar.width, militar.height);
+	console.log("Hola mundo!", mortero.width, mortero.height);
 	
-	pixi.x = app.screen.width / 2;
-	pixi.y = app.screen.height / 2;
+	militar.x = 120;
+	militar.y = 140;
 
-	app.stage.addChild(pixi);
+	mortero.x = 425;
+	mortero.y = 165;
+	mortero.angle = 38;
+
+	mortero.scale.set(0.1, 0.1);
+
+	app.stage.addChild(militar);
+	app.stage.addChild(mortero);
 });
