25b1070adaecd8db0477a3c8e82a842bbc3b2996
BenjaminEAdam <80794769+BenjaminEAdam@users.noreply.github.com>
2023-06-08 07:46:28 -0300

    Agregué el escalado, los márgenes y el contenedor.

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,60 @@
-import { Application, Assets, Sprite } from 'pixi.js'
+import { Application, Assets, Container, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1280,
+	height: 720
 });
 
 window.addEventListener("resize", ()=>{
 	console.log("resize done");
 	
-	const scaleWidth = Math.round(window.innerWidth / app.screen.width);
-	const scaleHeight = Math.round(window.innerHeight/app.screen.height);
+	const scaleWidth = window.innerWidth / app.screen.width;
+	const scaleHeight = window.innerHeight/app.screen.height;
 	const scaleScreen = Math.min(scaleWidth, scaleHeight);
 
 	const gameWhidth = Math.round(app.screen.width * scaleScreen);
 	const gameHeight = Math.round(app.screen.height * scaleScreen);
 
-	//const marginVertical = Math.floor((window.innerWidth - gameWhidth)/2);
-	//const marginHorizontal = Math.floor((window.innerHeight - gameHeight)/2);
+	const marginVertical = Math.floor((window.innerWidth - gameWhidth)/2);
+	const marginHorizontal = Math.floor((window.innerHeight - gameHeight)/2);
 
-	if(app.view.style){
-		app.view.style.width = gameWhidth + "px";
-		app.view.style.height = gameHeight + "px";
-	}
+	const canvas = app.view as HTMLCanvasElement;
 
-	
+	canvas.style.width = gameWhidth + "px";
+	canvas.style.height = gameHeight + "px";
 
+	canvas.style.marginLeft = marginVertical + "px";
+	canvas.style.marginRight = marginVertical + "px";
 
+	canvas.style.marginTop = marginHorizontal + "px";
+	canvas.style.marginBottom = marginHorizontal + "px";
 });
 window.dispatchEvent(new Event("resize"));
 
-
-Assets.add("clampy", "./clampy.png");
-Assets.add("pixi", "./pixi.png");
 Assets.add("militar", "./militar.png");
 Assets.add("mortero", "./mortero.png");
 
-Assets.load(["clampy", "pixi", "militar", "mortero"]).then(() => {
+Assets.load(["militar", "mortero"]).then(() => {
+	
+	const militar: Sprite = Sprite.from("militar");
+	militar.position.set(120,140);
+	militar.scale.set(1.3, 1.3);
 	
 	const mortero: Sprite = Sprite.from("mortero");
-	const militar: Sprite = Sprite.from("militar");
-	//const pixi: Sprite = Sprite.from("pixi");
-	//const clampy: Sprite = Sprite.from("clampy.png");
-	
-	console.log("Hola mundo!", militar.width, militar.height);
-	console.log("Hola mundo!", mortero.width, mortero.height);
-	
-	militar.x = 120;
-	militar.y = 140;
-
-	mortero.x = 425;
-	mortero.y = 165;
-	mortero.angle = 38;
-
+	mortero.position.set(505,192);
 	mortero.scale.set(0.1, 0.1);
+	mortero.angle = 40;
 
-	app.stage.addChild(militar);
-	app.stage.addChild(mortero);
+	const militarDisparando: Container = new Container();
+	militarDisparando.addChild(militar);
+	militarDisparando.addChild(mortero);
+	militarDisparando.scale.set(0.8,0.8);
+	militarDisparando.position.set(-150, 75);
+	militarDisparando.angle = -5;
+
+	app.stage.addChild(militarDisparando);
 });
