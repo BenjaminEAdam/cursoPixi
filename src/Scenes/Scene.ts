686e880b6328cd423da532b2440eb7b5dc326b18
BenjaminEAdam <benjaadam14@gmail.com>
2023-07-25 21:40:02 -0300

    Animación logo con gravedad

--- src/Scenes/Scene.ts
+++ src/Scenes/Scene.ts
@@ -5,11 +5,11 @@ import { UserInterfaceKeyboard } from "./UserInterfaceKeyboard";
 import { IActualizable } from "../Utils/IActualizable";
 import { LogoDeCarga } from "./LogoDeCarga";
 import { index } from "..";
+import { PhysicsContainer } from "../Game/PhysicsContainer";
 
 export class Scene extends Container implements IActualizable{
 
-    private logo: LogoDeCarga;
-    private direccion = 0;
+    private physicsLogo: PhysicsContainer;
 
     constructor(){
         super();
@@ -42,77 +42,58 @@ export class Scene extends Container implements IActualizable{
 
         const logoDeCarga = new LogoDeCarga();
         logoDeCarga.scale.set(0.5, 0.5);
-        logoDeCarga.position.set(1200, 650);
-        this.addChild(logoDeCarga);
         Ticker.shared.add(function(deltaFrame){
             logoDeCarga.update(Ticker.shared.deltaMS, deltaFrame);
         })
 
-        this.logo=logoDeCarga;
+        this.physicsLogo = new PhysicsContainer();
+        this.physicsLogo.position.set(1200, 650);
+        this.physicsLogo.speed.x = 100;
+        this.physicsLogo.speed.y = -280;
+        this.physicsLogo.acceleration.x = 0;
+        this.physicsLogo.acceleration.y = 60;
+        this.addChild(this.physicsLogo);
+        this.physicsLogo.addChild(logoDeCarga);
 
     }
-    
-    public update(_deltaTime: number, _deltaFrame: number): void {
-        
-        /*
 
-        Direccion 0: Diagonal hacia arriba y hacia la derecha
-        Direccion 1: Diagonal hacia arriba y hacia la izquierda
-        Direccion 2: Diagonal hacia abajo y hacia la izquierda
-        Direccion 3: Diagonal hacia abajo y hacia la derecha
+    public update(deltaTime: number, _deltaFrame: number): void {
 
-        */
+        console.log("Pos x:", Math.trunc(this.physicsLogo.x));
+        console.log("Tam x:", Math.trunc(index.screenWidth - this.physicsLogo.width));
 
+        console.log("Pos y:", Math.trunc(this.physicsLogo.y));
+        console.log("Tam y:", Math.trunc(index.screenHeight - this.physicsLogo.height));
 
-        if(this.logo.x == Math.trunc(index.screenWidth - this.logo.width) || this.logo.x == 0 ||
-            this.logo.y == Math.trunc(index.screenHeight - this.logo.height) || this.logo.y == 0){
-            if(this.logo.x == Math.trunc(index.screenWidth - this.logo.width) && this.direccion == 0){
-                this.direccion = 1;
-            }else if(this.logo.x == Math.trunc(index.screenWidth - this.logo.width) && this.direccion == 3){
-                this.direccion = 2;
-            }else if (this.logo.x == 0 && this.direccion == 1){
-                this.direccion = 0;
-            }else if (this.logo.x == 0 && this.direccion == 2){
-                this.direccion = 3;
-            }else if(this.logo.y == Math.trunc(index.screenHeight - this.logo.height) && this.direccion == 2){
-                this.direccion = 1;
-            }else if(this.logo.y == Math.trunc(index.screenHeight - this.logo.height) && this.direccion == 3){
-                this.direccion = 0;
-            }else if(this.logo.y == 0 && this.direccion == 0){
-                this.direccion = 3;
-            }else if(this.logo.y == 0 && this.direccion == 1){
-                this.direccion = 2;
-            }
+        // Si speed en Y negativa (-) y X positiva (+) logo va hacia arriba y hacia la derecha.
+        // Si speed en Y negativa (-) y X negativa (-) logo va hacia arriba y hacia la izquierda.
+        // Si speed en Y positiva (+) y X negativa (-) logo va hacia abajo y hacia la izquierda.
+        // Si speed en Y positiva (+) y X positiva (+) logo va hacia abajo y hacia la derecha.
+
+        if(this.physicsLogo.x >= (index.screenWidth - this.physicsLogo.width)){
+            this.physicsLogo.x = (index.screenWidth - this.physicsLogo.width);
+        }
+        if(this.physicsLogo.x <= 0){
+            this.physicsLogo.x = 0;
+        }
+        if(this.physicsLogo.y >= (index.screenHeight - this.physicsLogo.height)){
+            this.physicsLogo.y = (index.screenHeight - this.physicsLogo.height);
+        }
+        if(this.physicsLogo.y <= 0){
+            this.physicsLogo.y = 0;
         }
 
-        this.moverContainer(this.logo, this.direccion);
-
-        //Maquina de lag
-        /*for(let index = 0; index < 150000000; index++){
-            1+1;
-        }*/
-
-    }
-
-    private moverContainer(container : Container, direccion: Number): void{
-        switch (direccion){
-            case 0:
-                container.x++;
-                container.y--;
-                break;
-            case 1:
-                container.x--;
-                container.y--;
-                break;
-            case 2:
-                this.logo.x--;
-                this.logo.y++;
-                break;
-            case 3:
-                this.logo.x++;
-                this.logo.y++;
-                break;
+        if(this.physicsLogo.x == (index.screenWidth - this.physicsLogo.width) 
+        || (this.physicsLogo.x == 0)){
+            this.physicsLogo.speed.x = this.physicsLogo.speed.x * (-1); 
+        }else if(this.physicsLogo.y == (index.screenHeight - this.physicsLogo.height)
+        || (this.physicsLogo.y == 0)){
+            this.physicsLogo.speed.y = this.physicsLogo.speed.y * (-1); 
         }
 
+        const deltaTimeSeconds = deltaTime / 1000;
+        this.physicsLogo.update(deltaTimeSeconds);
+
     }
+
 }
